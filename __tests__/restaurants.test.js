82963d5214b3fe96e6adf6d95975ff57c6ebf22a
David Francisco <dfrancisco2677@gmail.com>
2022-08-26 10:33:44 -0700

    Created reviews and users tables, completed relationships between tables. Wrote test for reviews, created model and controller files.

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -3,6 +3,26 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  firstName: 'Flo',
+  lastName: 'Jo',
+  email: 'iam.speed@gmail.com',
+  password: 'running4ever'
+};
+
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+//   const agent = request.agent(app);
+//   const user = await UserService.create({ ...mockUser, ...userProps });
+
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
+
+
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -17,6 +37,19 @@ describe('backend-express-template routes', () => {
   });
 });
 
+it('/restaurants/:id should return a restaurant', async () => {
+  const res = await request(app).get('/api/v1/restaurants/1');
+  expect(res.body.id).toEqual('1');
+  expect(res.status).toEqual(200);
+});
+
+it('/restaurants/:id/reviews returns reviews for that restaurant', async() => {
+  const res = await request(app).get('/api/v1/restaurants/1');
+  expect(res.body).toHaveProperty('id', '1');
+  expect(res.body).toHaveProperty('name', 'Franks');
+  expect(res.body).toHaveProperty('type', 'American');
+  expect(res.body.reviews[0]).toHaveProperty('id', '1');
+});
 
 afterAll(() => {
   pool.end();
